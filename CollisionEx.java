cdc9c7e885ef366718332b5a34dae8a4dfa4a4b4
InquisitiveDev2016 <alihaider2011@live.ca>
2019-12-30 23:25:37 -0500

    Create CollisionEx.java

---
+++ CollisionEx.java
@@ -0,0 +1,32 @@
+package gameBasics;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class CollisionEx extends JFrame {
+
+    public CollisionEx() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Collision");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            CollisionEx ex = new CollisionEx();
+            ex.setVisible(true);
+        });
+    }
+}
